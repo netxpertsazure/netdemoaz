006e53ae8ac8d6b84eabf2e84f6b8f03bb03860f
matrix <info@netxperts.com>
2024-07-27 09:39:23 +0000

    Java Code 3rd

--- Sales.java
+++ Sales.java
@@ -2,3 +2,5 @@ boolean isJavaFun = true;
 boolean isFishTasty = false;
 System.out.println(isJavaFun);     // Outputs true
 System.out.println(isFishTasty);   // Outputs false
+int x = 10;
+System.out.println(x == 10); // returns true, because the value of x is equal to 10
